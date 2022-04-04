fb2dc52cd72267c0b6ea8bb7be229347a59030e2
Norge Fajardo <norge.fajardo@travelgenio.com>
2022-04-03 21:33:33 -0300

    Change entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-k6 run /tmp/script.js -o output-prometheus-remote -e MY_HOSTNAME=${HOSTNAME}
+#k6 run /tmp/script.js -o output-prometheus-remote -e MY_HOSTNAME=${HOSTNAME}
+k6 run /tmp/script.js --out influxdb=http://monitoring-influxdb.monitoring-influxdb.svc/demo
 
 exec "$@"
