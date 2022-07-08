f93a2625697b3a331fab225952175b04bb409e51
Norge Fajardo Vega <fajardovega@gmail.com>
2022-07-08 12:51:03 -0300

    test conectivity

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-k6 run /tmp/script.js -o influxdb="http://${INFLUXDBV1_SERVICE_HOST}:${INFLUXDBV1_SERVICE_PORT}/jam3"
+k6 run /tmp/script.js -o influxdb="http://${INFLUXDB_SERVICE_HOST}:${INFLUXDB_SERVICE_PORT}/jam3"
 
 exec "$@"
